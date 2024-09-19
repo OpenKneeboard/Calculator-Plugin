b38ea825cd5f5f9294f70be7d97b475f264843b1
Fred Emmott <git@fredemmott.com>
2024-09-19 15:05:33 -0500

    OKB: support multiple views

--- script.js
+++ script.js
@@ -1,58 +1,130 @@
 // Copyright (c) 2024 Fred Emmott
 // SPDX-License-Identifier: MIT
-function onNumber(buffer, value) {
-  if (buffer.textContent === '0') {
+
+const InstanceKind = Object.freeze({
+  None: Symbol('None'),
+  Standalone: Symbol('Standalone'),
+  First: Symbol('First'),
+  Additional: Symbol('Additional'),
+})
+
+let instanceKind = InstanceKind.None;
+const instanceId = crypto.randomUUID();
+
+class HistoryEntry {
+  constructor(lhs, op, rhs, result) {
+    this.lhs = lhs;
+    this.op = op;
+    this.rhs = rhs;
+    this.result = result;
+  }
+}
+
+class PageUI {
+  #id;
+  #bufferNode;
+  #stagingNode;
+  #historyNode;
+  #historyEntries = [];
+
+  constructor(node) {
+    this.#id = node.id;
+    this.#bufferNode = node.querySelector('.buffer');
+    this.#stagingNode = node.querySelector('.staging');
+    this.#historyNode = node.querySelector('.history');
+  }
+
+  pendingBinaryOp = null;
+  pendingBinaryLHS = null;
+
+  get id() {
+    return this.#id;
+  }
+
+  get buffer() {
+   return this.#bufferNode.textContent;
+  }
+
+  set buffer(value) {
+    this.#bufferNode.textContent = value;
+  }
+
+  get staging() {
+    return this.#stagingNode.textContent;
+  }
+
+  set staging(value) {
+    this.#stagingNode.textContent = value;
+  }
+
+  get history() {
+    return this.#historyEntries;
+  }
+
+  pushHistoryEntry(entry) {
+    let lhsNode = document.createElement('div');
+    lhsNode.textContent = entry.lhs;
+    lhsNode.className = "mono historyOperand historyValue lhs";
+    lhsNode.setAttribute('data-value', entry.lhs);
+
+    let opNode = document.createElement('div');
+    opNode.textContent = entry.op;
+    opNode.className = "mono historyOp";
+
+    let rhsNode = document.createElement('div');
+    rhsNode.textContent = entry.rhs;
+    rhsNode.className = "mono historyOperand historyValue rhs";
+    rhsNode.setAttribute('data-value', entry.rhs);
+
+    let resultNode = document.createElement('div')
+    resultNode.textContent = entry.result;
+    resultNode.className = "mono historyResult historyValue";
+    resultNode.setAttribute('data-value', entry.result);
+
+    for (const node of [lhsNode, rhsNode, resultNode]) {
+      node.addEventListener(
+        'click',
+        () => setBuffer(this, node.getAttribute('data-value'))
+      );
+    }
+
+    this.#historyEntries.push(entry);
+    this.#historyNode.prepend(lhsNode, opNode, rhsNode, resultNode);
+  }
+}
+
+function onNumber(page, value, sendToPeers = true) {
+  if (sendToPeers && window.OpenKneeboard?.SendMessageToPeers) {
+    OpenKneeboard.SendMessageToPeers({
+      kind: 'number',
+      sender: instanceId,
+      page: page.id,
+      value: value,
+    })
+  }
+
+  if (page.buffer === '0') {
     if (value === '0') {
       return;
     }
-    buffer.textContent = value;
+    page.buffer = value;
     return;
   }
 
-  buffer.textContent += value;
+  page.buffer += value;
 }
 
-function evalPending(page, history, staging, buffer) {
-  const op = page.nextBinaryOp;
+function evalPending(page) {
+  const op = page.pendingBinaryOp;
   if (!op) {
     return;
   }
-  page.nextBinaryOp = null;
+  page.pendingBinaryOp = null;
 
-  let rhs = buffer.textContent;
+  const rhs = page.buffer;
 
-  buffer.textContent = op.invoke(parseFloat(page.nextLHSOperand), parseFloat(rhs));
-  pushHistory(history, page.nextLHSOperand, op.label, rhs, buffer.textContent, buffer);
-}
-
-function pushHistory(history, lhs, op, rhs, result, buffer) {
-  let lhsNode = document.createElement('div');
-  lhsNode.textContent = lhs;
-  lhsNode.className = "mono historyOperand historyValue lhs";
-  lhsNode.setAttribute('data-value', lhs);
-
-  let opNode = document.createElement('div');
-  opNode.textContent = op;
-  opNode.className = "mono historyOp";
-
-  let rhsNode = document.createElement('div');
-  rhsNode.textContent = rhs;
-  rhsNode.className = "mono historyOperand historyValue rhs";
-  rhsNode.setAttribute('data-value', rhs);
-
-  let resultNode = document.createElement('div')
-  resultNode.textContent = result;
-  resultNode.className = "mono historyResult historyValue";
-  resultNode.setAttribute('data-value', result);
-
-  for (const node of [lhsNode, rhsNode, resultNode]) {
-    node.addEventListener(
-      'click',
-      () => buffer.textContent = node.getAttribute('data-value')
-    );
-  }
-
-  history.prepend(lhsNode, opNode, rhsNode, resultNode);
+  page.buffer = op.invoke(parseFloat(page.pendingBinaryLHS), parseFloat(rhs));
+  page.pushHistoryEntry(new HistoryEntry(page.pendingBinaryLHS, op.label, rhs, page.buffer));
 }
 
 class Op {
@@ -79,22 +151,22 @@ class BinaryOp extends Op {
   }
 }
 
-const BinaryOps = {
+const BinaryOps = Object.freeze({
   Plus: new BinaryOp('+', (a, b) => a + b),
   Minus: new BinaryOp('-', (a, b) => a - b),
   Multiply: new BinaryOp('×', (a, b) => a * b),
   Divide: new BinaryOp("÷", (a, b) => a / b),
-};
+});
 
-function onBinaryOp(page, history, staging, buffer, op) {
-  evalPending(page, history, staging, buffer);
+function onBinaryOp(page, op) {
+  evalPending(page);
 
-  page.nextBinaryOp = op;
-  page.nextLHSOperand = buffer.textContent;
+  page.pendingBinaryOp = op;
+  page.pendingBinaryLHS = page.buffer;
 
-  staging.textContent = `${buffer.textContent} ${op.label}`;
+  page.staging = `${page.buffer} ${op.label}`;
 
-  buffer.textContent = "0";
+  page.buffer = "0";
 }
 
 class UnaryOp extends Op {
@@ -109,27 +181,27 @@ class UnaryOp extends Op {
   }
 }
 
-const UnaryOps = {
+const UnaryOps = Object.freeze({
   Equals: new UnaryOp('=', (x) => x),
   Sqrt: new UnaryOp('√', Math.sqrt),
-};
+});
 
-function onUnaryOp(page, history, staging, buffer, op) {
-  const havePending = !!page.nextBinaryOp;
-  evalPending(page, history, staging, buffer);
+function onUnaryOp(page, op) {
+  const havePending = !!page.pendingBinaryOp;
+  evalPending(page);
 
-  const operand = parseFloat(buffer.textContent);
-  buffer.textContent = op.invoke(operand);
+  const operand = parseFloat(page.buffer);
+  page.buffer = op.invoke(operand);
   if (op.label !== '=' || !havePending) {
-    pushHistory(history, operand, op.label, '', buffer.textContent, buffer);
+    page.pushHistoryEntry(new HistoryEntry(operand, op.label, '', page.buffer));
   }
 
-  staging.textContent = ""; 
+  page.staging = "";
 }
 
 function appendPage() {
   const id = crypto.randomUUID();
-  appendPageWithID(id);
+  return appendPageWithID(id);
 }
 
 function appendPageWithID(id) {
@@ -139,20 +211,26 @@ function appendPageWithID(id) {
   page.classList.remove('hidden');
   document.getElementById('pagesRoot').appendChild(page);
   attachJSToPage(page);
+  return page;
 }
 
 function onOp(page, op, sendToPeers= true) {
-  const history = page.querySelector('.history');
-  const buffer = page.querySelector('.buffer');
-  const staging = page.querySelector('.staging');
+  if (sendToPeers && window.OpenKneeboard?.SendMessageToPeers) {
+    OpenKneeboard.SendMessageToPeers({
+      kind: 'op',
+      sender: instanceId,
+      page: page.id,
+      op: op,
+    });
+  }
 
   if (op in BinaryOps) {
-    onBinaryOp(page, history, staging, buffer, BinaryOps[op]);
+    onBinaryOp(page, BinaryOps[op]);
     return;
   }
 
   if (op in UnaryOps) {
-    onUnaryOp(page, history, staging, buffer, UnaryOps[op]);
+    onUnaryOp(page, UnaryOps[op]);
     return;
   }
 
@@ -160,34 +238,182 @@ function onOp(page, op, sendToPeers= true) {
   debugger;
 }
 
-function attachJSToPage(page) {
-  const buffer = page.querySelector('.buffer');
+function attachJSToPage(pageNode) {
+  const page = new PageUI(pageNode);
+  pageNode.okbCalculator = page;
 
-  for(const button of page.querySelectorAll("button.num")) {
+  for(const button of pageNode.querySelectorAll("button.num")) {
     button.addEventListener('click', () => {
-      onNumber(buffer, button.getAttribute('data-value'));
+      onNumber(page, button.getAttribute('data-value'));
     });
   }
 
-  page.querySelector('.backspace').addEventListener(
+  pageNode.querySelector('.backspace').addEventListener(
     'click',
-    () => {
-      buffer.textContent = buffer.textContent.substring(0, buffer.textContent.length - 1);
-      if (buffer.textContent === '') {
-        buffer.textContent = '0';
-      }
-    }
+    () => onBackspace(page)
   );
 
   for(const op of ['Plus', 'Minus', 'Multiply', 'Divide', 'Equals', 'Sqrt']) {
-    page.querySelector(`.op${op}`).addEventListener(
+    pageNode.querySelector(`.op${op}`).addEventListener(
       'click',
       () => onOp(page, op)
     );
   }
 }
 
-if (!window.OpenKneeboard) {
-  document.body.classList.add('dev');
-  appendPage();
+async function initializeNewOpenKneeboardTab() {
+  const page = appendPage();
+  await OpenKneeboard.SetPages([ { guid: page.id, pixelSize: { width: 768, height: 1024 }}]);
+  instanceKind = InstanceKind.First;
 }
+
+async function initializeExistingOpenKneeboardTab(pages) {
+  for (const page of pages) {
+    appendPageWithID(page.guid);
+  }
+  instanceKind = InstanceKind.Additional;
+
+  await OpenKneeboard.SendMessageToPeers({
+    kind: "bootstrapRequest",
+    sender: instanceId,
+  });
+}
+
+async function sendBootstrapData(sender) {
+  let response = {};
+  for (const node of document.querySelectorAll('.page')) {
+    if (node.id === 'pageTemplate') {
+      continue;
+    }
+
+    const page = node.okbCalculator;
+    let pageData = {
+      id: page.id,
+      buffer: page.buffer,
+      staging: page.staging,
+      history: page.history,
+    };
+
+    if (page.pendingBinaryOp) {
+      for (const [name, value] of BinaryOps) {
+        if (value === page.pendingBinaryOp) {
+          pageData.pendingBinaryOp = name;
+          pageData.pendingBinaryLHS = page.pendingBinaryLHS;
+          break;
+        }
+      }
+    }
+
+    response[page.id] = pageData;
+  }
+  await OpenKneeboard.SendMessageToPeers({
+    kind: 'bootstrapResponse',
+    recipient: sender,
+    response: response,
+  });
+}
+
+function onBackspace(page, sendToPeers = true) {
+  if (sendToPeers && window.OpenKneeboard?.SendMessageToPeers) {
+    OpenKneeboard.SendMessageToPeers({
+      kind: 'backspace',
+      sender: instanceId,
+      page: page.id,
+    })
+  }
+
+  page.buffer =  page.buffer.substring(0, page.buffer.length - 1);
+}
+
+function setBuffer(page, value, sendToPeers = true) {
+  if (sendToPeers && window.OpenKneeboard?.SendMessageToPeers) {
+    OpenKneeboard.SendMessageToPeers({
+      kind: 'setBuffer',
+      sender: instanceId,
+      page: page.id,
+      value: value,
+    })
+  }
+
+  page.buffer = value;
+}
+
+async function loadBootstrapData(data) {
+  const pages = Array.from(document.querySelectorAll('.page')).map(
+    (node) => node.okbCalculator
+  );
+
+  for (const page of pages) {
+    const pageData = data[page.id];
+    if (!pageData) {
+      continue;
+    }
+
+    page.buffer = pageData.buffer;
+    page.staging = pageData.staging;
+    for (const entry of pageData.history) {
+      page.pushHistoryEntry(entry);
+    }
+
+    if (!pageData.pendingBinaryOp) {
+      continue;
+    }
+
+    page.pendingBinaryOp = BinaryOps[pageData.pendingBinaryOp];
+    page.pendingBinaryLHS = pageData.pendingBinaryLHS;
+  }
+}
+
+async function onPeerMessage(ev) {
+  const message = ev.detail.message;
+
+  switch (message.kind) {
+    case 'bootstrapResponse':
+      if (message.recipient === instanceId) {
+        await loadBootstrapData(message.response);
+      }
+      return;
+    case 'op':
+      onOp(document.getElementById(message.page).okbCalculator, message.op, false);
+      return;
+    case 'number':
+      onNumber(document.getElementById(message.page).okbCalculator, message.value, false);
+      return;
+    case 'backspace':
+      onBackspace(document.getElementById(message.page).okbCalculator, false);
+      return;
+    case 'setBuffer':
+      setBuffer(document.getElementById(message.page).okbCalculator, message.value, false);
+      return;
+  }
+
+  if (instanceKind !== InstanceKind.First) {
+    return;
+  }
+
+  switch (message.kind) {
+    case 'bootstrapRequest':
+      await sendBootstrapData(message.sender);
+      return;
+  }
+}
+
+async function initOpenKneeboard() {
+  OpenKneeboard.addEventListener('peerMessage', onPeerMessage);
+  await OpenKneeboard.EnableExperimentalFeature("PageBasedContent", 2024073001);
+
+  const pages = await OpenKneeboard.GetPages();
+  if (pages.havePages) {
+    await initializeExistingOpenKneeboardTab(pages.pages);
+  } else {
+    await initializeNewOpenKneeboardTab();
+  }
+
+  document.body.classList.add('OpenKneeboard');
+}
+
+initOpenKneeboard().catch(() => {
+  instanceKind = InstanceKind.Standalone;
+  document.body.classList.add('fallbackUI');
+  appendPage();
+});
